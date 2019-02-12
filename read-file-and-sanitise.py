218f7f8bdd94c043fe096ca6c20e72542f4adaaf
Marco Tedone <marco.tedone@gmail.com>
2019-02-12 18:50:43 +0000

    Added the CSV module

--- read-file-and-sanitise.py
+++ read-file-and-sanitise.py
@@ -1,13 +1,15 @@
+import csv
 
+total = 0.0
 
 with open('Data/portfolio.csv', 'r') as f:
-    headers = next(f)           # Skips the first line
-    for line in f:
-        line = line.strip()     # Strip whitespace
-        parts = line.split(',')
-        parts[0] = parts[0].strip('"')
-        parts[1] = parts[1].strip('"')
-        parts[2] = int(parts[2])
-        parts[3] = float(parts[3])
-        print(parts)
+    rows = csv.reader(f)
+    headers = next(rows)           # Skips the first line
+    for row in rows:
+        row[2] = int(row[2])
+        row[3] = float(row[3])
+        print(row)
+        total +=  row[2]*row[3]
+
+print(total)
 
